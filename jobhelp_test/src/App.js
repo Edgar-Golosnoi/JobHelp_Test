e64881d123ba76fa5977a8deea3911ea9a202585
Edgar <edgar.golosnoi@yandex.ru>
2023-02-25 11:28:13 +0300

    release 2

--- jobhelp_test/src/App.js
+++ jobhelp_test/src/App.js
@@ -4,21 +4,36 @@ import './App.css';
 // import Popupform from './components/PopupForm/Popupform';
 import Navbar from './components/Navbar/Navbar';
 // import Registration from './components/RegistrationForm/Registration';
+import Body from './components/Body/Body';
 
 
 
 function App() {
   return (
-    <div className='App'>
+    <div className='App-container'>
         <Navbar />
         <div className='body-main-text'>
-          <h1>Подберём резюме для ваших вакансий и обзвоним кандидатов </h1>
+          <h1>Подберём резюме для ваших вакансий <br /> и обзвоним кандидатов </h1>
         </div>
         <div className='body-text'>
-            <h1>Проведём детальный анализ вакансии</h1>
+            <h4 className='text-1'>Проведём детальный анализ вакансии</h4>
+            <h1 className='center-column1'></h1>
+            <h4 className='text-2'>Организуем ручной подбор с учётом ваших требований и пожеланий</h4>
+            <h1 className='center-column2'></h1>
+            <h4 className='text-3'>Соберём актуальные резюме с ведущих job-ресурсов</h4>
+        </div>
+        <div className='body-content'>
+          <h1 className='title'>Оставьте заявку, и менеджер ответит в течение 30 минут</h1>
+          <input className='userName' placeholder='Ваше имя' type='text'></input>
+          <input className='userTel' placeholder='Ваше телефон' type='text'></input>
+          <input className='aboutSerch' placeholder='Какого сотрудника вы ищите' type='text'></input>
+          <button className='submitForm' type='text'>Заказать звонок</button>
+          <h2 className='title'>Нажимая на кнопку вы даете согласие <u>на обработку персональных данных и соглашаетесь с политикой конфидециальности</u></h2>
         </div>
         <img className="card-img" src="../image1.png" alt="JobHelp"/>
+        <Body />
     </div>
+    
   )
 }
 
---
+++ jobhelp_test/src/components/Body/Body.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+export default function Body() {
+  return (
+    <div className="row">
+    <div className="col-sm-6">
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Доступ к базам job-ресурсов</h5>
+          <p className="card-text">Найдём лучших кандидатов среди 74 млн резюме из баз популярных</p>
+          <a href="#" className="btn">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div className="col-sm-6">
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Дешевле, чем кадровое агентство</h5>
+          <p className="card-text">Наши услуги стоят в 3-4 раза дешевле, чем у кадровых агентств.</p>
+          <a href="#" className="btn">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div className="col-sm-6">
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Помощь персонального менеджера</h5>
+          <p className="card-text">Найдём лучших кандидатов среди 74 млн резюме из баз популярных.</p>
+          <a href="#" className="btn">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div className="col-sm-6">
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Высокая скорость работы</h5>
+          <p className="card-text">Присылаем первые резюме в течение двух рабочих дней после начала работы. Резюме</p>
+          <a href="#" className="btn">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div className="col-sm-6">
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Весь поиск как на ладони</h5>
+          <p className="card-text">Гарантируем высокое качество работ. Наблюдайте за всеми</p>
+          <a href="#" className="btn">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div className="col-sm-6">
+    <div className="card">
+    <div className="card-body">
+      <h5 className="card-title">Большой опыт в поиске кандидатов </h5>
+      <p className="card-text">Гарантируем высокое качество работ. Наблюдайте за всеми</p>
+      <a href="#" className="btn">Go somewhere</a>
+      </div>
+      </div>
+      </div>
+    </div>
+  )
+}
+
